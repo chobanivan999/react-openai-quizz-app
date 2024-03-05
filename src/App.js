7160303aaa00175df646cc38c88c6cb8546e3367
chobanivan999 <chobanivan999@gmail.com>
2024-03-05 15:45:20 +0200

    update backend server link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 //data will be the string we send from our server
 const apiCall = () => {
-  axios.get('http://localhost:8080/video').then((data) => {
+  axios.get('https://express-openai-quizz-server.vercel.app:8080/video').then((data) => {
     //this console.log will be in our frontend console
     console.log(data)
   })
